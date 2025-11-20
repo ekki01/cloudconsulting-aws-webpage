a7b8c77785fc5015c9772746c2745e4700ee73f3
ekki01 <ekki01@t-online.de>
2025-11-20 18:36:25 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,25 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    
-    $to = "ekkehard.barth@cloudconsulting-aws.de";
-    $subject = "Neue Nachricht von Ihrer Website";
-    
-    $email_content = "Name: $name\n";
-    $email_content .= "E-Mail: $email\n\n";
-    $email_content .= "Nachricht:\n$message\n";
-    
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    
-    if (mail($to, $subject, $email_content, $headers)) {
-        echo "Vielen Dank! Ihre Nachricht wurde erfolgreich gesendet.";
-    } else {
-        echo "Es tut uns leid, beim Senden Ihrer Nachricht ist ein Fehler aufgetreten.";
-    }
-} else {
-    echo "Zugriff verweigert";
-}
-?>
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "you@example.com";
+$mail->Password = "password";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("dave@example.com", "Dave");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
